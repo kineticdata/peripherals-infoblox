92f4c6309ae2bd51285151c5156367759366d1a7
rehmc <chad.rehm@kineticdata.com>
2019-06-18 09:20:45 -0500

    initial commit

---
+++ handlers/infoblox_record_create_v1/test/simple_input.rb
@@ -0,0 +1,16 @@
+{
+  'info' =>
+  {
+    'host_url' => '',
+    'username' => '',
+    'password' => '',
+    'enable_debug_logging' => 'Yes'
+  },
+  'parameters' =>
+  {
+    'name' => '',
+    'zone' => '',
+    'ip_address' => '',
+    'create_ptr_record' => 'true'
+    }
+}
